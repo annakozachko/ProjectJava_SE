88dab98ad518065dd33438cabef1d3060adda081
annakozachko <annakozachko@gmail.com>
2022-02-04 18:55:17 +0200

    q

---
+++ src/main/java/ua/intita/qa/homework21/Car.java
@@ -0,0 +1,24 @@
+package ua.intita.qa.homework21;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.ToString;
+
+import java.util.UUID;
+
+
+@Data
+@AllArgsConstructor
+@ToString
+public class Car {
+    private String carId;
+    private String name;
+    private String model;
+    private String year;
+
+    public Car() {
+        carId = UUID.randomUUID().toString();
+    }
+
+
+}
